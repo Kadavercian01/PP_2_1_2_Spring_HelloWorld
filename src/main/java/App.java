2987625e0db2676da4f8007b09dd5314a84c4ec4
Kadavercian01 <indigo.gamer.life@gmail.com>
2023-07-27 09:57:13 +0300

    add new bean (cat), add test equals

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+        Cat beanThree =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanThree.getMessage());
+        Cat beanFore =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanFore.getMessage());
+        System.out.println(bean.equals(beanTwo));
+        System.out.println(beanThree.equals(beanFore));
     }
 }
